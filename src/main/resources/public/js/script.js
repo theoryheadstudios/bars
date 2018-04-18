6e756f46fb2a7bd0d5de4667285b3cbd89df66c1
freddyacevedo <freddyace1993@gmail.com>
2018-04-17 23:38:41 -0400

    first commit

--- src/main/resources/public/js/script.js
+++ src/main/resources/public/js/script.js
@@ -164,23 +164,6 @@ var modalCloseButton = document.getElementById("modalCloseButton");
 var mainModalCloseButton = document.getElementById("mainModalCloseButton");
 
 var loginModalContent = "<center><h1>Welcome back!</h1></center><center><form action='/action_page.php'><input id='email' type='email' name='email' placeholder='Email' style='width:300px'><br><input id='password' type='password' name='password' placeholder='Password' style='width:300px'><br><br><br><br><center><a class='button' id='loginEventButton' >Let's Go!</a></center></form></center>";
-//THIS IS HTML TO BE INSERTED INTO THE FIRST MODAL *********
-var userTypeForm = "<div class='renterCard'><center><img src='../images/rent.png' alt='Avatar' style='width:100%;'></center><div class='renterContainer'><center><h1><b>I'm here to rent stuff!</b></h1></center><center><p>Sign me up!</p></center></div></div><div class='ownerCard'><img src='../images/owner.jpg' alt='Avatar' style='width:100%'><div class='ownerContainer'><center><h1><b>I have stuff to rent!</b></h1></center><center><p>Sign me up!</p></center></div></div>"
-userTypeForm+="<br><br><br><br><br><br>";
-
-var signUpForm = "<center><div><center><p>Tell us about you!</p></center><center><p>Please fill out the fields below so we can match you up with neightbors who can help</p></center>";
-signUpForm+="<form action='/action_page.php'><input id='firstname' type='text' name='firstname' placeholder='First' style='width:150px'><input id='lastname' type='text' name='lastname' placeholder='Last' style='width:155px'><br><input type='text' name='email' placeholder='Email' style='width:304px'><br><input type='text' name='addressLine1' placeholder='Address' style='width:304px'><br><input type='text' name='addressLine2' placeholder='Address Line 2' style='width:304px'><br><input id='zipcode' type='text' name='zipCode' placeholder='Zip Code' style='width:150px'><input id='neighborhood' type='text' name='Neighborhood' placeholder='Neighborhood' style='width:150px'><br><input type='text' name='username' placeholder='Desired Username' style='width:304px'><br><input type='text' name='password' placeholder='Password' style='width:304px'><br><input type='text' name='passwordMatch' placeholder='Verify Password' style='width:304px'><br><br><br></form>";
-signUpForm+="</div></center>";
-
-
-var categoryTable = "<center><p>First, let's get to know you a little better...</p><center><center><p>Begin by selecting your interests...</p></center>"
-categoryTable+="<table id='customers'><tr><td><label class='Modalcontainer'>Clothing<input type='checkbox'><span class='checkmark'></span></label></td></tr><tr><td><label class='Modalcontainer'>Health & Fitness<input type='checkbox'><span class='checkmark'></span>"
-categoryTable+="</label></td></tr><tr><td><label class='Modalcontainer'>Lawn & Gardening<input type='checkbox'><span class='checkmark'></span></label></td></tr><tr><td><label class='Modalcontainer'>Electronics<input type='checkbox'><span class='checkmark'></span></label></td></tr><tr><td><label class='Modalcontainer'>Camera Equipment<input type='checkbox'><span class='checkmark'></span></label></td></tr><tr><td><label class='Modalcontainer'>Outdoor & Hiking<input type='checkbox'><span class='checkmark'>"
-categoryTable+="</span></label></td></tr><tr><td><label class='Modalcontainer'>Musical Instruments<input type='checkbox'><span class='checkmark'></span></label></td></tr><tr><td><label class='Modalcontainer'>Jewelery<input type='checkbox'><span class='checkmark'></span></label></td></tr><tr><td><label class='Modalcontainer'> Industrial & Heavy Vehicles<input type='checkbox'><span class='checkmark'></span></label></td></tr><tr><td><label class='Modalcontainer'>Costumes & Cosplay<input type='checkbox'><span class='checkmark'></span></label></td></tr>"
-categoryTable+="</table><br><br>"
-
-
-var neighborhoodTable = "<label class='RadioButtonContainer'>Brookhaven, GA<input type='radio' checked='checked' name='radio'><span class='RadioCheckmark'></span></label><label class='RadioButtonContainer'>Buckhead, GA<input type='radio' name='radio'><span class='RadioCheckmark'></span></label><label class='RadioButtonContainer'>Marietta, GA<input type='radio' name='radio'><span class='RadioCheckmark'></span></label><label class='RadioButtonContainer'>Smyrna, GA<input type='radio' name='radio'><span class='RadioCheckmark'></span></label>"
 
 mainModalCloseButton.onclick=function(){
   modal.style.display="none";
@@ -208,9 +191,7 @@ loginButton.onclick = function(){
 
 
 joinWeecloButton.onclick = function() {
- //   $(window).scrollTop(0);
- //  modal.style.display = "block";
- // document.getElementById("modalContent").innerHTML = userTypeForm;
+
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "http://localhost:3001/listOfAllUsers");
     xhttp.setRequestHeader("Content-type", "application/json");
@@ -227,26 +208,6 @@ joinWeecloButton.onclick = function() {
     }
 
 }
-
-
-// signUpButton.onclick = function() {
-//     modal.style.display = "block";
-//    // document.getElementById("modalContent").innerHTML = userTypeForm;
-//     var xhr= new XMLHttpRequest();
-//     xhr.open('GET', 'userType.html', true);
-//     xhr.onreadystatechange= function() {
-//         if (this.readyState!==4) return;
-//         if (this.status!==200) return; // or whatever error handling you want
-//         document.getElementById('modalContent').innerHTML= this.responseText;
-//     };
-//     xhr.send();
-// }
-
-// When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modal.style.display = "none";
-//     console.console.log("Clicked Span");
-// }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -283,11 +244,6 @@ else if(innerButtonClickCount==1||stepState==1){
   console.log("Click Count: "+innerButtonClickCount);
   console.log("Step State: "+stepState)
 
-
-
-// document.getElementById("modalContent").innerHTML = signUpForm;
-// document.getElementById('step2').className+="active";
-
     var xhr= new XMLHttpRequest();
     xhr.open('GET', 'modalForm.html', true);
     xhr.onreadystatechange= function() {
@@ -302,8 +258,7 @@ else if(innerButtonClickCount==1||stepState==1){
 else if(innerButtonClickCount==2||stepState==2){
 console.log("Click Count: "+innerButtonClickCount);
 console.log("Step State: "+stepState)
-// document.getElementById('modalContent').innerHTML = categoryTable;
-// document.getElementById('step3').className+="active";
+
     var xhr= new XMLHttpRequest();
     xhr.open('GET', 'categoryTable.html', true);
     xhr.onreadystatechange= function() {
@@ -319,8 +274,6 @@ console.log("Step State: "+stepState)
 else if(innerButtonClickCount==3||stepState==3){
 console.log("Click Count: "+innerButtonClickCount);
 console.log("Step State: "+stepState)
-// document.getElementById('modalContent').innerHTML = neighborhoodTable;
-// document.getElementById('step4').className+="active";
 
     var xhr= new XMLHttpRequest();
     xhr.open('GET', 'neighborhoodTable.html', true);
@@ -334,12 +287,7 @@ console.log("Step State: "+stepState)
           innerButtonClickCount++;
     console.log("Click Count is now: "+innerButtonClickCount)
 }
-
-
-
 }
-
-
 
 function restartModal(){
     console.clear();
@@ -348,35 +296,10 @@ function restartModal(){
   innerButtonClickCount = 0;
   stepState=0;
   console.log("click count: "+innerButtonClickCount);
-  // document.getElementById('step2').className-="active";
-  // document.getElementById('step3').className-="active";
-  // document.getElementById('step4').className-="active";
 
     onClickNext();
 }
-// modalNextButton.onclick = function(){
-//     modalState=1;
-//     innerButtonClickCount++;
-//   if(innerButtonClickCount==1){
-//     modalState=1;
-//     console.log(innerButtonClickCount);
-//   document.getElementById('yup').innerHTML = "";
-//   document.getElementById('step2').className+="active";
-//  }
-// }
-// else if(innerButtonClickCount==2){
-//   modalState=2;
-//   console.log(innerButtonClickCount);
-//   document.getElementById('yup').innerHTML = "";
-//   document.getElementById('step3').className+="active";
-// }
-// else if(innerButtonClickCount==3){
-//   modalState=3;
-//   console.log(innerButtonClickCount);
-//   document.getElementById('yup').innerHTML = ""
-//   document.getElementById('step4').className+="active";
-// }
-// }
+
 
 function isRenter(){
     document.getElementById('renterCardID').style.backgroundColor = "#90EE90";
@@ -420,6 +343,14 @@ function completeSignUp() {
 
 
     }, 3000);
+}
+
+function serialize(){
+    console.log("****************************")
+    // $('form').serialize()
+    // console.log(JSON.stringify($('form').serializeArray()));
+    var value = document.getElementById('firstname').value
+    console.log("first name: "+value);
 }
 
 //END OF MODAL
