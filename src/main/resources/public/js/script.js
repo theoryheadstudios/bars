4dcdbf9e82ca7a1b8f34180e17f47573b398fc20
Giandre Acosta <gacosta@g.clemson.edu>
2018-06-16 17:36:34 -0400

    checkboxes look smoother, user checkbox data is saved, listen button changes

--- src/main/resources/public/js/script.js
+++ src/main/resources/public/js/script.js
@@ -35,146 +35,211 @@ var MainController = function($scope, $http){
   };
 
 
-
-var next = document.getElementById("nextBtn")
-var back = document.getElementById("backBtn")
 var video = document.getElementById("myVideo");
-var menuButton = document.getElementById("menuButton")
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
-var menuCloseButton = document.getElementById("menuCloseButton");
 
 // When the user clicks the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
+    // if the right menu is already displaying, then close it, open the modal, and increment click counter on menu 
+    if(document.getElementById('rightMenu').style.width !== "0px" ){
+      document.getElementById('rightMenu').style.width = "0px";
+      document.getElementById('menuButton').clickcount = Number(document.getElementById('menuButton').clickcount)+1;
+    }
 }
+   
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
+    
 }
+
+// if User presses 'Escape' then close the modal
+document.onkeydown = function(event) {
+  event = event || window.event;
+  if (event.keyCode == 27) {
+    if(modal.style.display === "block"){
+      modal.style.display = "none";
+    }
+  }
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target === modal) {
         modal.style.display = "none";
     }
-}
-
-menuButton.onclick=function () {
-    document.getElementById("rightMenu").style.display = "block";
-    console.log("button pressed.")
-}
-menuCloseButton.onclick=function () {
-    document.getElementById("rightMenu").style.display = "none";
-}
-
-var nextPage = {
-  'check' : [document.getElementById("first").innerHTML, document.getElementById("step2form").innerHTML, 
-document.getElementById("step3form").innerHTML, document.getElementById("step4form").innerHTML],
-'ul' : [document.getElementById("step1"), document.getElementById("step2"),
-document.getElementById("step3"), document.getElementById("step4")]
-}
-
-
-// next button will allow user to progress to next page
-var count = 0;
-next.onclick = function (){
-  if(count < 3){
-  $scope.innerButtonClickCount++;
-    // console.log($scope.innerButtonClickCount);
-    // console.log(nextPage.check[count])
-  document.getElementById('yup').innerHTML = nextPage.check[count+1]
-  nextPage.ul[count+1].className="active"
-  console.log(arr);
-  count++;
-  console.log("COUNT: " + count)
-  }
-  else{
-    document.getElementById('yup').innerHTML = nextPage.check[count]
-    nextPage.ul[count].className="active"
-  }
-}
-
-// The back button will allow the user to go back a single page
-back.onclick = function (){
-  if(count !== 0){
-    count--;
-    console.log("COUNT: " + count)
-  document.getElementById('yup').innerHTML = nextPage.check[count]
-  nextPage.ul[count+1].className="inactive"
-  $scope.innerButtonClickCount++;
-  }
-  if(count === 0){
-  nextPage.ul[0].className="active"
-  }
 }};
-
-// function Forward(){
-//   nextPage.push("stp2")
-//   $scope.innerButtonClickCount++;
-//   if($scope.innerButtonClickCount===1){
-//     console.log($scope.innerButtonClickCount);
-//   document.getElementById('yup').innerHTML = document.getElementById("step2form").innerHTML
-//   document.getElementById('step2').className+="active";
-//   console.log(arr);
-// }
-// else if($scope.innerButtonClickCount===2){
-//   console.log($scope.innerButtonClickCount);
-//   document.getElementById('yup').innerHTML = document.getElementById("step2form").innerHTML
-//   document.getElementById('step3').className+="active";
-// }
-// else if($scope.innerButtonClickCount===3){
-//   console.log($scope.innerButtonClickCount);
-//   document.getElementById('yup').innerHTML =  document.getElementById("step4form").innerHTML
-//   document.getElementById('step4').className+="active";
-// }
-// }
-
-
-
-
-//   if($scope.innerButtonClickCount===4){
-//     console.log($scope.innerButtonClickCount);
-//   document.getElementById('yup').innerHTML = document.getElementById("step2form").innerHTML
-//   document.getElementById('step4').className+="inactive";
-
-// }
-// else if($scope.innerButtonClickCount===5){
-//   console.log(document.activeElement)
-//   console.log($scope.innerButtonClickCount);
-//   document.getElementById('yup').innerHTML = document.getElementById("step4form").innerHTML
-//   document.getElementById('step3').className+="inactive";
-// }
-// else if($scope.innerButtonClickCount===6){
-//   console.log(document.activeElement)
-//   console.log($scope.innerButtonClickCount);
-//   document.getElementById('yup').innerHTML =  document.getElementById("step2form").innerHTML
-//   document.getElementById('step2').className+="inactive";
-// }
-// }};
-
 
 app.controller("MainController", ["$scope","$http", MainController]);
 }());
 
 
-// save values of each checkbox
+
+/* 
+---------------------------------------------------------------------------------------
+------------------- FUNCTIONS CREATED OUTSIDE OF THE CONTROLLER ------------------------ 
+----------------------------------------------------------------------------------------
+*/
+
+
+/* this array will be used to switch to different pages when user clicks on 'next' or 'back' buttons
+ and will mark the progress bar as active or inactive */
+var switchPage = {
+  'check' : [document.getElementById('firstForm').innerHTML, document.getElementById("step2form").innerHTML, 
+             document.getElementById("step3form").innerHTML, document.getElementById("step4form").innerHTML],
+  'ul'    : [document.getElementById("step1"), document.getElementById("step2"),
+             document.getElementById("step3"), document.getElementById("step4")]
+}
+/* next button will allow user to progress to next page 
+  -
+  The next button will grab htlm code form the array of 'switchPage' to switch to the next page,
+  mark the progress bar steps as active, and grab checkbox values from when
+  the user had chosen if they moved to other pages before
+*/
+var count = 0;
+function onClickNext (){
+  if(count < 3){
+    document.getElementById('firstForm').innerHTML = switchPage.check[count+1]
+    document.getElementById('nextBtn').style.float = "right"
+    switchPage.ul[count+1].className="active"
+    console.log(arr);
+    count++;
+  }
+  else{
+    document.getElementById('firstForm').innerHTML = switchPage.check[count]
+    switchPage.ul[count].className="active"
+  }
+  //do not display back button on first page
+  //display on any other page
+  // on last page, display 'Finish' button, not the 'next' button
+  if(count == 0){
+    document.getElementById("backBtn").style.display = "none";
+  }else if(count == 1){
+    document.getElementById("backBtn").style.display = "block";
+  }else if(count == 3){
+    document.getElementById("submitBtn").style.display = "Block";
+    document.getElementById("nextBtn").style.display = "none";
+  }
+  getSavedChecks();
+}
+
+/* The back button will allow the user to go back a single page
+  -
+  the back button will grab html code from the array of 'switchPage' to go back to previous page,
+  mark the progress bar steps as inactive and grab the checkbox values from before to display
+*/
+function onClickBack(){
+  if(count !== 0){
+    count--;
+    console.log("COUNT: " + count)
+  document.getElementById('firstForm').innerHTML = switchPage.check[count]
+  switchPage.ul[count+1].className="inactive"
+  }
+  if(count === 0){
+  switchPage.ul[0].className="active"
+  document.getElementById("backBtn").style.display = "none";
+  document.getElementById('nextBtn').style.float = "none"
+  }else{
+    document.getElementById("submitBtn").style.display = "none";
+    document.getElementById("nextBtn").style.display = "block";
+  }
+  getSavedChecks();
+}
+
+
+/* function to save values of each clicked checkbox
+  -
+  if the checkbox is checked, then save the value
+  otherwise, if unchecked, remove it from the array
+*/
 var arr = new Array();
 function saveData(checkbox){
   if(checkbox.checked){
+    arr = arr.filter(e => e !== checkbox.value)
     arr.push(checkbox.value);
   }else{
-    arr.pop(checkbox.value);
+    arr = arr.filter(e => e !== checkbox.value)
   }
-
-  // if(typeof(Storage) !== "undefined"){
-  //   localStorage.setItem("check", checkbox.value)
-  //   console.log(localStorage.getItem("check"));
-  // }else{
-  //   document.getElementById("yup").innerHTML = "Sorry, your browser does not support Web Storage...";
-  // }
 console.log("TagName: " + checkbox.tagName + " The checkbox is: " + checkbox.checked);
 }
 
+/* function to call to toggle the menu button
+  -
+  if menu is first clicked, then open it up
+  else if menu is clicked thereafter on 2nd hit, open
+  otherwise, close it.
+ */
+function toggleMenu(menu){
+  if(typeof(menu.clickcount) === "undefined"){
+    menu.clickcount = 1;
+    document.getElementById("rightMenu").style.width = "250px";
+  }
+  else if(menu.clickcount % 2 === 0){
+    menu.clickcount = Number(menu.clickcount)+1;
+    document.getElementById("rightMenu").style.width = "250px";
+  }
+  else{
+    menu.clickcount = Number(menu.clickcount)+1;
+    document.getElementById("rightMenu").style.width = "0";
+  }
+}
+
+/* function to call whenever the 'x' is clicked in the menuButton
+    -
+    if user clicks on the 'x' in the menu button, then close it
+    and increment the counter of the menuButton so that next time
+    the user clicks, they can toggle it and open again
+*/
+function closeMenu() {
+  document.getElementById("rightMenu").style.width = "0";
+  document.getElementById("menuButton").clickcount += 1;
+}
+
+
+var checkboxes = ["EDM", "House", "Metal", "Djent", "Dubstep", "Pop", "Hip-Hop", "RnB", "Soul", "Country"];
+var checkboxes2 = ["Male", "Female", "Other"];
+var checkboxes3 = ["Something", "Goes", "Here"];
+/* Function to re-check all enabled checkboxes that the 
+  user provided when filling out the form
+  -
+  if the each table respectively is reached, then call this function
+  and check any boxes that the user had checked before 
+ */
+function getSavedChecks(){
+  for(i = 0; i < arr.length; ++i){
+    if(count == 0){
+      if(checkboxes.includes(arr[i])){
+        document.getElementById(arr[i]).checked = true;
+      }
+    }else if(count == 1){
+      if(checkboxes2.includes(arr[i])){
+        document.getElementById(arr[i]).checked = true;
+      }
+    }else if(count == 2){
+      if(checkboxes3.includes(arr[i])){
+        document.getElementById(arr[i]).checked = true;
+      }
+    }
+  }
+}
+
+var subButton = document.getElementById('submitBtn');
+subButton.addEventListener('mouseleave', saveJoinPageInput, false); 
+// subButton.addEventListener('click', saveJoinPageInput, false); 
+
+function saveJoinPageInput(){
+    var nameField = document.getElementById('first').value;
+    var result = document.getElementById('result');
+    
+    if (nameField.length < 3) {
+        // result.textContent = 'Username must contain at least 3 characters';
+        alert('Username must contain at least 3 characters');
+    } else {
+        result.textContent = 'Your username is: ' + nameField;
+        // alert(nameField);
+    }
+ }
