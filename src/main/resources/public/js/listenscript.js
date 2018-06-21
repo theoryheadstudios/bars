702e713702b94bc3abe1250842cd6c7ce51068ce
Giandre Acosta <gacosta@g.clemson.edu>
2018-06-20 20:49:38 -0400

    Added Listening UI layout

--- src/main/resources/public/js/listenscript.js
+++ src/main/resources/public/js/listenscript.js
@@ -1,16 +1,133 @@
-window.onload = randomizeImage();
-
-function randomizeImage(){
-    // Randomize images
-
-    // var myPix = new Array("images/Dark-Flashes-Neon.jpg","images/wonderworks.jpg","images/motel.jpg","images/hongkong.jpg");
-    // var randomNum = Math.floor(Math.random() * myPix.length);
-    // document.body.background = myPix[randomNum];
-
-    /* or we could name our images by number and randomize that way */
-    var totalCount = 6;
-    var num = Math.ceil( Math.random() * totalCount );
-    document.body.background = 'images/'+"listen"+num+'.jpg';
+// window.onload = randomizeImage();
 
 
-}
+
+// function randomizeImage(){
+//     // Randomize images
+
+//     // var myPix = new Array("images/Dark-Flashes-Neon.jpg","images/wonderworks.jpg","images/motel.jpg","images/hongkong.jpg");
+//     // var randomNum = Math.floor(Math.random() * myPix.length);
+//     // document.body.background = myPix[randomNum];
+
+//     /* or we could name our images by number and randomize that way */
+//     var totalCount = 6;
+//     var num = Math.ceil( Math.random() * totalCount );
+//     document.body.background = 'images/'+"listen"+num+'.jpg';
+
+
+// }
+
+$(document).ready(function() {
+	
+	setTimeout(function(){
+		$('body').addClass('loaded');
+        $('h1').css('color','#ffffff');
+	}, 500);
+});
+
+// var audio_player = $("#audio-player");
+// var play_button = $('#play');
+// var progress_bar = $("#progressbar");
+// var time = $("#time");
+// var mute_button = $('#mute');
+// var volume_bar = $('#volume');
+// var more_info = $('#more-info-box');
+// var info_tray = $("#info-tray");
+// var player = document.getElementById('player');
+// var duration = 0;
+// var volume = 0.75;
+
+// player.onloadedmetadata = function() {
+//     duration = player.duration;
+//     progress_bar.progressbar("option", { 'max' : duration });
+// };
+
+// player.load();
+// player.volume = 0.75;
+// player.addEventListener("timeupdate", function() {
+//     progress_bar.progressbar('value', player.currentTime);
+//     time.text(getTime(player.currentTime));
+// }, false);
+
+// function getTime(t) {
+//     var m=~~(t/60), s=~~(t % 60);
+//     return (m<10?"0"+m:m)+':'+(s<10?"0"+s:s);
+// }
+
+// function getProgressBarClickInfo(progress_bar, e) {
+//     var offset = progress_bar.position();
+//     var x = e.pageX - offset.left; // or e.offsetX (less support, though)
+//     var y = e.pageY - offset.top;  // or e.offsetY
+//     var max = progress_bar.progressbar("option", "max");
+//     var value = x * max / progress_bar.width();
+
+//     return { x: x, y: y, max: max, value: value };
+// }
+
+// volume_bar.progressbar({
+//     value : player.volume*100,
+// });
+
+// volume_bar.click(function(e) {
+//     var info = getProgressBarClickInfo($(this), e);
+//     volume_bar.progressbar('value', info.value);
+//     player.volume = info.value / info.max;
+// });
+
+// progress_bar.progressbar({
+//     value : player.currentTime,
+// });
+
+// progress_bar.click(function(e) {
+//     var info = getProgressBarClickInfo($(this), e);
+//     player.currentTime = player.duration / info.max * info.value;
+// });
+
+// play_button.click(function() {
+//     player[player.paused ? 'play' : 'pause']();
+//     $(this).toggleClass("fa-play", !player.paused);
+//     $(this).toggleClass("fa-pause", player.paused);
+// });
+
+// mute_button.click(function() {
+//     if (player.volume == 0) {
+//         player.volume = volume;
+//     } else {
+//         volume = player.volume;
+//         player.volume = 0;
+//     }
+
+//     volume_bar.progressbar('value', player.volume * 100);
+
+//     $(this).toggleClass("fa-volume-up", player.volume != 0);
+//     $(this).toggleClass("fa-volume-off", player.volume == 0);
+// });
+
+// more_info.click(function() {
+//     audio_player.animate({
+//         height: (audio_player.height() == 50) ? 100 : 50
+//     }, 1000);
+// });
+
+
+function addClass( element, classname ) {
+    if (element.classList)
+      element.classList.add(classname);
+    else
+      element.className += ' ' + classname;
+    }
+    
+    function removeClass( classname, element ) {
+        var cn = element.className;
+        var rxp = new RegExp( "\\s?\\b"+classname+"\\b", "g" );
+        cn = cn.replace( rxp, '' );
+        element.className = cn;
+    }
+    
+    var progressBarEl = document.getElementById("progress-bar");
+    var controlsPlayEl = document.getElementById("controls-play");
+    
+    function play() {
+      addClass(progressBarEl, "play");
+      addClass(controlsPlayEl, "play");
+    }
